06dc65a3a713f8739f77a7c651416a46a93b362a
Abhishek262539 <bgabhishek056@gmail.com>
2024-11-26 12:39:41 +0530

    Create Grade.c

---
+++ Grade.c
@@ -0,0 +1,26 @@
+include <stdio.h> 
+  int main()
+{
+int pre;
+printf("enter the pre");
+Scanf("%d",&pre);
+if(pre >= 90)
+{  
+Printf("grade; A /n^ prime prime");
+}
+Else if(pre >= 75)
+{
+printf("grade; B/n");
+}
+else if(poe >= 50)
+{
+printf ("grade; c/n")
+} 
+else
+{
+printf ("grade; F/n");
+}
+return 0;
+}
+
+4
