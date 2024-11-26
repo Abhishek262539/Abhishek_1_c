32bd922ab2c8d661fc1d2da3142f7969a4837a8e
Abhishek262539 <bgabhishek056@gmail.com>
2024-11-26 12:47:09 +0530

    Create Positive or negative.c

---
+++ Positive or negative.c
@@ -0,0 +1,20 @@
+# inclued <stdio.h>
+int main()
+{
+int num;
+printf ("enter the num");
+scanf ("%d",num);
+if (num>0);
+{
+Printf("The number is positive");
+}
+else if(num<0)
+{
+paintf("the number is ni negative");
+}
+else
+{
+printf("The number is zero");
+}
+return 0;
+}
